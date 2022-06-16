bc29918a9eb082117e711f54598867f874745c34
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-06-16 16:13:03 -0300

    Added basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // decleration
-    public CharacterController characterController;
+    public Rigidbody2D characterController;
+
+    private float gravityValue = 9.81f;
     private float playerSpeed = 5f;
+    private float jumpHeight = 10f;
+
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +22,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckGrounded();
         PlayerMove();
+        PlayerJump();
     }
 
-    // player movement
+    void CheckGrounded()
+    {
+        // player has to be on "Igonre Raycast Layer" for this to work, might need work.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1);
+        if (hit.collider != null)
+        {
+            //Debug.Log("isGrouned");
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    // -------------------------------- Player Movement ----------------------------------------
     void PlayerMove()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        characterController.Move(move * Time.deltaTime * playerSpeed);
+        Vector2 moveHorizontal = new Vector2(Input.GetAxis("Horizontal"), 0);
+        transform.Translate(moveHorizontal * Time.deltaTime * playerSpeed);
+    }
+
+    void PlayerJump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            characterController.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+        }
     }
 }
